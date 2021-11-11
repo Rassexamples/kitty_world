6ddf1ee480e3699423bf88a7739cc856beb06937
Rass <rasool.rahmanzade@gmail.com>
2021-11-11 12:42:07 +0100

    updates, RandomInRange from 'extension' to 'abs' class

--- lib/util/extensions.dart
+++ lib/util/extensions.dart
@@ -1,6 +1,6 @@
 import 'dart:math';
 
-extension RandomInRange on int {
+abstract class RandomInRange {
   static int randomInRange(int min, int max) {
     final _random = Random();
     return min + _random.nextInt(max - min);
